c2e305960ba443352a12cd4acc88cae6901dedcb
Will <doggy.huang@gmail.com>
2017-12-25 02:34:09 +0800

    Support for Hyper-V provider ( Not Ready Yet )
    TODO: Wait for Vagrant & Hyper-V support assign static IP address!
    
    Limitations - Hyper-V Provider - Vagrant by HashiCorp
    https://www.vagrantup.com/docs/hyperv/limitations.html
    1. 目前Vagrant無法自動選擇網路介面，必須手動選擇
    2. 目前Vagrant強迫自動設定Static IP或自動設定NAT介面。但在vagrant up的時候會顯示抓到哪個IP地址。

--- Vagrantfile
+++ Vagrantfile
@@ -1,9 +1,11 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
 
 Vagrant.require_version ">= 1.7.0"
 
 Vagrant.configure("2") do |config|
-  config.vm.provider "virtualbox"
-  config.vm.box = "bento/ubuntu-16.04"
+  config.vm.provider "hyperv"
+  config.vm.box = "kmm/ubuntu-xenial64"
 
   master = 1
   node = 2
@@ -13,9 +15,13 @@ Vagrant.configure("2") do |config|
     id   = (mid <= node) ? mid : (mid - node)
     config.vm.define "#{name}#{id}" do |n|
       n.vm.hostname = "#{name}#{id}"
-      ip_addr = "192.16.35.#{private_count}"
-      n.vm.network :private_network, ip: "#{ip_addr}",  auto_config: true
-      n.vm.provider "virtualbox" do |v|
+      n.vm.synced_folder ".", "/vagrant", smb_host: "<you_host_ip>", smb_username: "<username>", smb_password: "<password>"
+      # Hyper-V provider has some limitation that it can't assign static by now!
+      # The following n.vm.network settings will totally ignored by Vagrant with Hyper-V provider.
+      ip_addr = "192.168.137.#{private_count}"
+      n.vm.network "private_network", ip: "#{ip_addr}"
+      n.vm.provider "hyperv" do |v|
+        v.vmname = "#{name}#{id}"
         v.memory = 2048
         v.cpus = 2
       end
--- hacks/deps.sh
+++ hacks/deps.sh
@@ -2,6 +2,8 @@
 #
 # Program: Initial vagrant.
 # History: 2017/12/23 Kyle.b Release
+# History: 2017/12/25 Will
+# - Fixed Hyper-V problem
 
 
 function set_hosts() {
@@ -9,9 +11,9 @@ cat <<EOF > ~/hosts
 127.0.0.1   localhost
 ::1         localhost
 
-192.16.35.10 node1
-192.16.35.11 node2
-192.16.35.12 master1
+192.168.137.10 node1
+192.168.137.11 node2
+192.168.137.12 master1
 
 EOF
 }
@@ -20,22 +22,43 @@ set -e
 HOST_NAME=$(hostname)
 OS_NAME=$(awk -F= '/^NAME/{print $2}' /etc/os-release | grep -o "\w*"| head -n 1)
 
+# Due to Vagrant's Limited Networking on Hyper-V, IP address have to change manually after VM deployed.
+# https://www.vagrantup.com/docs/hyperv/limitations.html
+#sudo sed -ie 's/iface eth0 inet dhcp//g' /etc/network/interfaces
+#sudo sh -c 'echo "iface eth0 inet static" >> /etc/network/interfaces'
+#sudo sh -c 'echo "netmask 255.255.255.0" >> /etc/network/interfaces'
+#sudo sh -c 'echo "gateway 192.168.137.1" >> /etc/network/interfaces'
+#if [ ${HOST_NAME} == "node1" ]; then
+#  sudo sh -c 'echo "address 192.168.137.10" >> /etc/network/interfaces'
+#fi  
+#if [ ${HOST_NAME} == "node1" ]; then
+#  sudo sh -c 'echo "address 192.168.137.11" >> /etc/network/interfaces'
+#fi  
+#if [ ${HOST_NAME} == "master1" ]; then
+#  sudo sh -c 'echo "address 192.168.137.12" >> /etc/network/interfaces'
+#fi  
+
+
+
 if [ ${HOST_NAME} == "master1" ]; then
-  sudo sed -i 's/us.archive.ubuntu.com/tw.archive.ubuntu.com/g' /etc/apt/sources.list
+  sudo sed -i 's/at.archive.ubuntu.com/tw.archive.ubuntu.com/g' /etc/apt/sources.list
   sudo apt-add-repository -y ppa:ansible/ansible
   sudo apt-get update && sudo apt-get install -y ansible git sshpass python-netaddr libssl-dev
   
   yes "/root/.ssh/id_rsa" | sudo ssh-keygen -t rsa -N ""
-  HOSTS="192.16.35.10 192.16.35.11 192.16.35.12"
-  for host in ${HOSTS}; do
-    sudo sshpass -p "vagrant" ssh -o StrictHostKeyChecking=no vagrant@${host} "sudo mkdir -p /root/.ssh"
-    sudo cat /root/.ssh/id_rsa.pub | \
-         sudo sshpass -p "vagrant" ssh -o StrictHostKeyChecking=no vagrant@${host} "sudo tee /root/.ssh/authorized_keys"
-  done
-  cd /vagrant
+  #HOSTS="192.168.137.10 192.168.137.11 192.168.137.12"
+  #for host in ${HOSTS}; do
+  #  sudo sshpass -p "vagrant" ssh -o StrictHostKeyChecking=no vagrant@${host} "sudo mkdir -p /root/.ssh"
+  #  sudo cat /root/.ssh/id_rsa.pub | sudo sshpass -p "vagrant" ssh -o StrictHostKeyChecking=no vagrant@${host} "sudo tee -a /root/.ssh/authorized_keys"
+  #done
+  #cd /vagrant
   set_hosts
   sudo cp ~/hosts /etc/
 else
   set_hosts
   sudo cp ~/hosts /etc/
 fi
+
+# TODO: When networking restart, the `vagrant up` process will stop!
+# sudo /etc/init.d/networking restart
+
